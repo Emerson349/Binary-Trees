a20e68370b68048bd16a8d1a29f3c0e81c639222
Emerson349 <emersonhenriquesulpinoaraujo@gmail.com>
2025-02-13 18:56:51 -0300

    Update RbTree.java

--- rbTree/RbTree.java
+++ rbTree/RbTree.java
@@ -1,5 +1,14 @@
 package rbTree; // Incompleta
 
+/*
+    Aluno: Emerson Henrique Sulpino de Araújo
+    Email: emerson.henrique.sulpino.araujo@ccc.ufcg.edu.br
+    Matrícula: 123210141
+    User do GitHub: Emerson349
+
+    Implementação de uma Árvore AVL
+*/
+
 public class RbTree {
     private Node root; // Nó raiz da árvore
     private Node nil; // Nó NIL (representa folhas)
